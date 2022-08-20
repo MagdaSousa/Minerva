5623eb4949479832772dc811794f134a968c696a
MagdaSousa <magdasousadejesus@gmail.com>
2022-08-20 03:14:37 -0300

    include new models

---
+++ src/domains/models/income_groups/invome_groups.py
@@ -0,0 +1,23 @@
+from src.common.Enums import TablesNames
+from pydantic import validator
+from src.domains import Base, Column, Integer, String, relationship, ForeignKey
+
+
+class IncomeGroups(Base):
+    __tablename__ = TablesNames.period.value
+
+    PeriodID = Column(Integer, primary_key=True)
+    ResearchYear = Column(String(50), nullable=False)
+
+    @validator('PeriodID')
+    def field_country_name_cannot_be_null(cls, country_name):
+        if not country_name.replace(" ", ""):
+            raise ValueError('field country_name cannot be null')
+
+    @validator('ResearchYear')
+    def field_reference_year_cannot_be_null(cls, country_code):
+        if not country_code.replace(" ", ""):
+            raise ValueError('field country_code cannot be null')
+
+    def __repr__(self):
+        return f"IncomeGroups(PeriodID={self.countryID!r},  ResearchYear={self.ResearchYear!r})"
